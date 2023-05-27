d2802c0b5ecdf1cbbe1790c0d1011ad7dcf46fd7
yashwanthi-m <manchalayashwanthiofficial@gmail.com>
2023-05-27 17:06:31 +0530

    Initial Commit

---
+++ FoodNet/FoodNetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodNetApp.swift
+//  FoodNet
+//
+//  Created by Yashwanthi Manchala on 27/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodNetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
